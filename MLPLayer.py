3b986e868758d9807d50cdee4ada9b4120049871
AnNguyen0494 <59415225+AnNguyen0494@users.noreply.github.com>
2020-01-02 06:57:59 +0700

    Update MLPLayer.py

--- MLPLayer.py
+++ MLPLayer.py
@@ -91,6 +91,7 @@ class MLP():
                 #########################################################
                 #2. EVALUATE DATA WITH R2 SCORE FUNCTION
                 pred = self.YPred.eval(feed_dict={self.X:X_test})
+                logger.write('Y predict: {}' + str(pred))
                 Y_pred = [x[0] for x in pred]
                 Y_test = [x[0] for x in Y_test]
                 g = 0
